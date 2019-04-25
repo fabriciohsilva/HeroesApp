157b30dffd2e8cbe345ab970a2bbbff47dded2a3
Fabrício Henrique <fabricioh.silva@live.com>
2019-04-24 23:24:26 -0300

    merge thaysa into fabricio
    merging changes of both branch's for test

--- app/src/main/java/br/com/fabriciohsilva/heroesapp/view/form/FormActivity.kt
+++ app/src/main/java/br/com/fabriciohsilva/heroesapp/view/form/FormActivity.kt
@@ -14,9 +14,9 @@ import kotlinx.android.synthetic.main.activity_form.*
 import kotlinx.android.synthetic.main.loading.*
 import android.content.Intent
 import android.graphics.BitmapFactory
-import android.provider.MediaStore
 import android.widget.ImageView
 import android.graphics.Bitmap
+import android.provider.MediaStore
 import android.util.Base64
 import java.io.ByteArrayOutputStream
 import java.io.ByteArrayInputStream
@@ -162,21 +162,21 @@ class FormActivity : AppCompatActivity() {
         var weakness = true
 
         if (etName.editText?.length() === 0) {
-            etName.error = getText(R.string.nameRequired)
+            etName.error = getText(R.string.name_required)
             name = false
         } else {
             etName.error = null
         }
 
         if (etPower.editText?.length() === 0) {
-            etPower.error = getText(R.string.powerRequired)
+            etPower.error = getText(R.string.power_required)
             power = false
         } else {
             etPower.error = null
         }
 
         if (etWeakness.editText?.length() === 0) {
-            etWeakness.error = getText(R.string.weaknessRequired)
+            etWeakness.error = getText(R.string.weakness_required)
             weakness = false
         } else {
             etWeakness.error = null
